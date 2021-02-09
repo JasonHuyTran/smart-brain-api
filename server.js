0c77586c55a981823a3314b4eedd8ec7a9db363a
Jason Tran <jaycehuytran@gmail.com>
2021-02-08 20:07:35 -0800

    edited connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
 const register = require('./controllers/register.js');
 const signin = require('./controllers/signin.js');
@@ -11,12 +12,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const db = require('knex')({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
+const db = knex({
+  client: 'pg',
+  connection: {
+      connectionString: process.env.DATABASE_URL,
       ssl: true
-    }
+  }
 });
 
 app.get('/', (req, res) => res.send("it is working!"));
